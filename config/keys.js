0bf917e8d70c1555e471ba5982b1222bc31fa1af
Koushik4010 <98331437+Koushik4010@users.noreply.github.com>
2023-04-21 13:34:25 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -2,12 +2,12 @@
 
 module.exports={
     google:{
-        clientID:'408601177776-ct1qiai2nsct3uv6dnphllattar10kbr.apps.googleusercontent.com',
-        clientSecret:'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',
+        clientID:'<client id>',
+        clientSecret:'<client secret>',
         callbackURL: 'http://localhost:3000/auth/google/callback'
     },
     mongodb:{
-        dbURI:'mongodb://localhost:27017/blogverse'
+        dbURI:'<mongodb connecting string>'
     },
     session:{
         cookieKey:'theblogzisawesome',
